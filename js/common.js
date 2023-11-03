f4f7b08e898eb420e23ce646a6b6103e992a7882
quangvan1210 <quangnvgcd191035@fpt.edu.vn>
2023-11-03 15:01:51 +0700

    editcode +checkout code

--- js/common.js
+++ js/common.js
@@ -117,5 +117,45 @@ class CommonFooter extends HTMLElement {
       `;
     }
   }
+  class CommonCss extends HTMLElement {
+    constructor() {
+      super();
+    }
+    connectedCallback() {
+      this.innerHTML = `
+      <link rel="stylesheet" href="/css/bootstrap.min.css" type="text/css">
+      <link rel="stylesheet" href="/css/font-awesome.min.css" type="text/css">
+      <link rel="stylesheet" href="/css/elegant-icons.css" type="text/css">
+      <link rel="stylesheet" href="/css/magnific-popup.css" type="text/css">
+      <link rel="stylesheet" href="/css/nice-select.css" type="text/css">
+      <link rel="stylesheet" href="/css/owl.carousel.min.css" type="text/css">
+      <link rel="stylesheet" href="/css/slicknav.min.css" type="text/css">
+      <link rel="stylesheet" href="/css/style.css" type="text/css">
+      <script src="../../js/common.js"></script>
+      `;
+    }
+  }
+  class CommonjS extends HTMLElement {
+    constructor() {
+      super();
+    }
+    connectedCallback() {
+      this.innerHTML = `
+      <script src="/js/jquery-3.3.1.min.js"></script>
+      <script src="/js/bootstrap.min.js"></script>
+      <script src="/js/jquery.nice-select.min.js"></script>
+      <script src="/js/jquery.nicescroll.min.js"></script>
+      <script src="/js/jquery.magnific-popup.min.js"></script>
+      <script src="/js/jquery.countdown.min.js"></script>
+      <script src="/js/jquery.slicknav.js"></script>
+      <script src="/js/mixitup.min.js"></script>
+      <script src="/js/owl.carousel.min.js"></script>
+      <script src="/js/main.js"></script>
+      `;
+    }
+  }
+  customElements.define("common-css", CommonCss);
+  customElements.define("common-js", CommonJs);
+
   customElements.define("common-footer", CommonFooter);
   customElements.define("common-header", CommonHeader);
