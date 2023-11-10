d61c6b3a10c428a94f75caef543dc440a9bcb245
VuHuong <vuhuong51101@gmail.com>
2023-11-10 15:08:55 +0700

    TT-28 list product BE

--- js/common.js
+++ js/common.js
@@ -1,9 +1,9 @@
 class CommonFooter extends HTMLElement {
     constructor() {
-      super();
+        super();
     }
     connectedCallback() {
-      this.innerHTML = `
+        this.innerHTML = `
       
 <link rel="stylesheet" href="../CSS/footer.css">
 <footer class="footer">
@@ -65,14 +65,14 @@ class CommonFooter extends HTMLElement {
 </footer>
       `;
     }
-  }
-  class CommonHeader extends HTMLElement {
+}
+class CommonHeader extends HTMLElement {
     constructor() {
-      super();
+        super();
     }
     connectedCallback() {
-        
-      this.innerHTML = `
+
+        this.innerHTML = `
       <link rel="stylesheet" href="../CSS/header.css">
       <header class="header">
     <div class="container">
@@ -98,17 +98,19 @@ class CommonFooter extends HTMLElement {
                         </li>
                         <li><a href="../blog.html">Blog</a></li>
                         <li><a href="../contact.html">Contacts</a></li>
+                        
+
                     </ul>
                 </nav>
             </div>
             <div class="col-lg-3 col-md-3">
-                <div class="header__nav__option">
-                    <a href="#" class="search-switch"><img src="../img/icon/search.png" alt=""></a>
-                    <a href="#"><img src="/img/icon/heart.png" alt=""></a>
-                    <a href="#"><img src="/img/icon/cart.png" alt=""> <span>0</span></a>
-                    <div class="price">$0.00</div>
-                </div>
+            <div class="header__nav__option">
+                <a href="#" class="search-switch"><img src="../img/icon/search.png" alt=""></a>
+                <a href="#"><i class="fa-solid fa-heart icon_header"></i></a>
+                <a href="#"><i class="fa-solid fa-cart-shopping icon_header"></i><span>0</span></a>
+                <a href="/Customer/LoginAndRegister/Login/Login.html"><i class="fa-solid fa-right-to-bracket icon_header"></i></a>                    
             </div>
+        </div>
         </div>
         <div class="canvas__open"><i class="fa fa-bars"></i></div>
     </div>
@@ -116,13 +118,13 @@ class CommonFooter extends HTMLElement {
   
       `;
     }
-  }
-  class CommonCss extends HTMLElement {
+}
+class CommonCss extends HTMLElement {
     constructor() {
-      super();
+        super();
     }
     connectedCallback() {
-      this.innerHTML = `
+        this.innerHTML = `
       <link rel="stylesheet" href="/css/bootstrap.min.css" type="text/css">
       <link rel="stylesheet" href="/css/font-awesome.min.css" type="text/css">
       <link rel="stylesheet" href="/css/elegant-icons.css" type="text/css">
@@ -134,28 +136,200 @@ class CommonFooter extends HTMLElement {
       <script src="../../js/common.js"></script>
       `;
     }
-  }
-  class CommonJs extends HTMLElement {
+}
+class CommonSidebar extends HTMLElement {
     constructor() {
-      super();
+        super();
     }
     connectedCallback() {
-      this.innerHTML = `
-      <script src="/js/jquery-3.3.1.min.js"></script>
-      <script src="/js/bootstrap.min.js"></script>
-      <script src="/js/jquery.nice-select.min.js"></script>
-      <script src="/js/jquery.nicescroll.min.js"></script>
-      <script src="/js/jquery.magnific-popup.min.js"></script>
-      <script src="/js/jquery.countdown.min.js"></script>
-      <script src="/js/jquery.slicknav.js"></script>
-      <script src="/js/mixitup.min.js"></script>
-      <script src="/js/owl.carousel.min.js"></script>
-      <script src="/js/main.js"></script>
+        this.innerHTML = `
+    <body>
+    <section class="shop spad">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="shop__sidebar">
+                        <div class="shop__sidebar__search">
+                            <form action="#">
+                                <input type="text" placeholder="Search...">
+                                <button type="submit"><span class="icon_search"></span></button>
+                            </form>
+                        </div>
+                        <div class="shop__sidebar__accordion">
+                            <div class="accordion" id="accordionExample">
+                                <div class="card">
+                                    <div class="card-heading">
+                                        <a data-toggle="collapse" data-target="#collapseOne">Categories</a>
+                                    </div>
+                                    <div id="collapseOne" class="collapse show" data-parent="#accordionExample">
+                                        <div class="card-body">
+                                            <div class="shop__sidebar__categories">
+                                                <ul class="nice-scroll">
+                                                    <li><a href="#">Men (20)</a></li>
+                                                    <li><a href="#">Women (20)</a></li>
+                                                    <li><a href="#">Bags (20)</a></li>
+                                                    <li><a href="#">Clothing (20)</a></li>
+                                                    <li><a href="#">Shoes (20)</a></li>
+                                                    <li><a href="#">Accessories (20)</a></li>
+                                                    <li><a href="#">Kids (20)</a></li>
+                                                    <li><a href="#">Kids (20)</a></li>
+                                                    <li><a href="#">Kids (20)</a></li>
+                                                </ul>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="card">
+                                    <div class="card-heading">
+                                        <a data-toggle="collapse" data-target="#collapseTwo">Branding</a>
+                                    </div>
+                                    <div id="collapseTwo" class="collapse show" data-parent="#accordionExample">
+                                        <div class="card-body">
+                                            <div class="shop__sidebar__brand">
+                                                <ul>
+                                                    <li><a href="#">Louis Vuitton</a></li>
+                                                    <li><a href="#">Chanel</a></li>
+                                                    <li><a href="#">Hermes</a></li>
+                                                    <li><a href="#">Gucci</a></li>
+                                                </ul>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="card">
+                                    <div class="card-heading">
+                                        <a data-toggle="collapse" data-target="#collapseThree">Filter Price</a>
+                                    </div>
+                                    <div id="collapseThree" class="collapse show" data-parent="#accordionExample">
+                                        <div class="card-body">
+                                            <div class="shop__sidebar__price">
+                                                <ul>
+                                                    <li><a href="#">$0.00 - $50.00</a></li>
+                                                    <li><a href="#">$50.00 - $100.00</a></li>
+                                                    <li><a href="#">$100.00 - $150.00</a></li>
+                                                    <li><a href="#">$150.00 - $200.00</a></li>
+                                                    <li><a href="#">$200.00 - $250.00</a></li>
+                                                    <li><a href="#">250.00+</a></li>
+                                                </ul>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="card">
+                                    <div class="card-heading">
+                                        <a data-toggle="collapse" data-target="#collapseFour">Size</a>
+                                    </div>
+                                    <div id="collapseFour" class="collapse show" data-parent="#accordionExample">
+                                        <div class="card-body">
+                                            <div class="shop__sidebar__size">
+                                                <label for="xs">xs
+                                                    <input type="radio" id="xs">
+                                                </label>
+                                                <label for="sm">s
+                                                    <input type="radio" id="sm">
+                                                </label>
+                                                <label for="md">m
+                                                    <input type="radio" id="md">
+                                                </label>
+                                                <label for="xl">xl
+                                                    <input type="radio" id="xl">
+                                                </label>
+                                                <label for="2xl">2xl
+                                                    <input type="radio" id="2xl">
+                                                </label>
+                                                <label for="xxl">xxl
+                                                    <input type="radio" id="xxl">
+                                                </label>
+                                                <label for="3xl">3xl
+                                                    <input type="radio" id="3xl">
+                                                </label>
+                                                <label for="4xl">4xl
+                                                    <input type="radio" id="4xl">
+                                                </label>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="card">
+                                    <div class="card-heading">
+                                        <a data-toggle="collapse" data-target="#collapseFive">Colors</a>
+                                    </div>
+                                    <div id="collapseFive" class="collapse show" data-parent="#accordionExample">
+                                        <div class="card-body">
+                                            <div class="shop__sidebar__color">
+                                                <label class="c-1" for="sp-1">
+                                                    <input type="radio" id="sp-1">
+                                                </label>
+                                                <label class="c-2" for="sp-2">
+                                                    <input type="radio" id="sp-2">
+                                                </label>
+                                                <label class="c-3" for="sp-3">
+                                                    <input type="radio" id="sp-3">
+                                                </label>
+                                                <label class="c-4" for="sp-4">
+                                                    <input type="radio" id="sp-4">
+                                                </label>
+                                                <label class="c-5" for="sp-5">
+                                                    <input type="radio" id="sp-5">
+                                                </label>
+                                                <label class="c-6" for="sp-6">
+                                                    <input type="radio" id="sp-6">
+                                                </label>
+                                                <label class="c-7" for="sp-7">
+                                                    <input type="radio" id="sp-7">
+                                                </label>
+                                                <label class="c-8" for="sp-8">
+                                                    <input type="radio" id="sp-8">
+                                                </label>
+                                                <label class="c-9" for="sp-9">
+                                                    <input type="radio" id="sp-9">
+                                                </label>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="card">
+                                    <div class="card-heading">
+                                        <a data-toggle="collapse" data-target="#collapseSix">Tags</a>
+                                    </div>
+                                    <div id="collapseSix" class="collapse show" data-parent="#accordionExample">
+                                        <div class="card-body">
+                                            <div class="shop__sidebar__tags">
+                                                <a href="#">Product</a>
+                                                <a href="#">Bags</a>
+                                                <a href="#">Shoes</a>
+                                                <a href="#">Fashio</a>
+                                                <a href="#">Clothing</a>
+                                                <a href="#">Hats</a>
+                                                <a href="#">Accessories</a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <script src="/js/jquery-3.3.1.min.js"></script>
+    <script src="/js/bootstrap.min.js"></script>
+    <script src="/js/jquery.nice-select.min.js"></script>
+    <script src="/js/jquery.nicescroll.min.js"></script>
+    <script src="/js/jquery.magnific-popup.min.js"></script>
+    <script src="/js/jquery.countdown.min.js"></script>
+    <script src="/js/jquery.slicknav.js"></script>
+    <script src="/js/mixitup.min.js"></script>
+    <script src="/js/owl.carousel.min.js"></script>
+    <script src="/js/main.js"></script>
+    <script src="https://kit.fontawesome.com/c7429944f7.js" crossorigin="anonymous"></script>
+</body>
       `;
     }
-  }
-  customElements.define("common-css", CommonCss);
-  customElements.define("common-js", CommonJs);
+}
+customElements.define("common-sidebar", CommonSidebar);
 
-  customElements.define("common-footer", CommonFooter);
-  customElements.define("common-header", CommonHeader);
+customElements.define("common-css", CommonCss);
+customElements.define("common-footer", CommonFooter);
+customElements.define("common-header", CommonHeader);
