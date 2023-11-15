27da81b691d1b8d5c9604df06ca690b6e06ff846
HaT7 <tuanhagcd19902@fpt.edu.vn>
2023-11-16 00:33:09 +0700

    Sidebar filter category, search, and fix some bugs on login

---
+++ .history/js/productList_20231115104554.js
@@ -0,0 +1,209 @@
+
+document.addEventListener("DOMContentLoaded", function() {
+    const productList = document.getElementById("product-list");
+    const apiUrl = "http://localhost:4001/api/products";
+    fetch(apiUrl, {
+            mode: 'cors',
+            headers: {
+                'Access-Control-Allow-Origin': '*'
+            }
+        })
+        .then((response) => {
+            return response.json();
+        })
+        .then((data) => {
+            data.products.forEach((product) => {
+                const productItem = document.createElement("div");
+                productItem.className = "col-lg-4 col-md-6 col-sm-6";
+                productItem.innerHTML = `
+                    <div class="product__item">
+                       <div class="product__item__pic">
+                            <img src= "${product.image}"/>
+
+                            <ul class="product__hover">  
+                                <li>
+                                    <a href="#"><img src="/img/icon/heart.png" alt=""></a>
+                                </li>
+
+                                <li>
+                                    <a href="#"><img src="/img/icon/compare.png" alt=""> <span>Compare</span></a>
+                                </li>
+
+                                <li>
+                                    <a href="#"><img src="/img/icon/search.png" alt=""></a>
+                                </li>
+                            </ul>
+                        </div>
+
+                        <div class="product__item__text">
+
+                            <h6>${product.name}</h6>
+
+                            <a href="#" class="add-cart">+ Add To Cart</a>
+
+                            <div class="rating">
+                                <i class="fa fa-star-o"></i>
+                                <i class="fa fa-star-o"></i>
+                                <i class="fa fa-star-o"></i>
+                                <i class="fa fa-star-o"></i>
+                                <i class="fa fa-star-o"></i>
+                            </div>
+
+                            <h5>$${product.price}</h5>
+
+                            <div class="product__color__select">
+                                <label for="pc-4">
+                                    <input type="radio" id="pc-4">
+                                </label>
+
+                                <label class="active black" for="pc-5">
+                                    <input type="radio" id="pc-5">
+                                </label>
+
+                                <label class="grey" for="pc-6">
+                                    <input type="radio" id="pc-6">
+                                </label>
+                            </div>
+                        </div>
+                    </div>
+                `;
+                productList.appendChild(productItem);
+            });
+        })
+        .catch((error) => {
+            console.error("Error fetching product data:", error);
+        });
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    const productList = document.getElementById("collapseOne");
+    const apiUrl = "http://localhost:4001/api/products";
+    fetch(apiUrl, {
+            mode: 'cors',
+            headers: {
+                'Access-Control-Allow-Origin': '*'
+            }
+        })
+        .then((response) => {
+            return response.json();
+        })
+        .then((data) => {
+            console.log(data);
+            data.products.forEach((product) => {
+                const productItem = document.createElement("div");
+                productItem.className = "collapse show";
+                productItem.innerHTML = `
+                  <div class="card-body-category">
+                      <div class="shop__sidebar__categories">
+                          <ul class="nice-scroll">
+                              <li onclick="filterProducts">${product.category}</li>
+                          </ul>
+                      </div>
+                  </div>
+                `;
+                productList.appendChild(productItem);
+            });
+        })
+        .catch((error) => {
+            console.error("Error fetching product data:", error);
+        });
+});
+
+function filterProducts(selectedCategory) {
+    const filteredProducts = products.filter(product => product.category === selectedCategory);
+
+    renderProducts(filteredProducts);
+}
+
+function renderProducts(products) {
+    productListDiv.innerHTML = '';
+
+    document.addEventListener("DOMContentLoaded", function() {
+        const productList = document.getElementById("product-list");
+        const apiUrl = "http://localhost:4001/api/products";
+        fetch(apiUrl, {
+                mode: 'cors',
+                headers: {
+                    'Access-Control-Allow-Origin': '*'
+                }
+            })
+            .then((response) => {
+                return response.json();
+            })
+            .then((data) => {
+                data.products.forEach((product) => {
+                    const productItem = document.createElement("div");
+                    productItem.className = "col-lg-4 col-md-6 col-sm-6";
+                    productItem.innerHTML = `
+                    <div class="product__item">
+                       <div class="product__item__pic">
+                            <img src= "${product.image}"/>
+
+                            <ul class="product__hover">  
+                                <li>
+                                    <a href="#"><img src="/img/icon/heart.png" alt=""></a>
+                                </li>
+
+                                <li>
+                                    <a href="#"><img src="/img/icon/compare.png" alt=""> <span>Compare</span></a>
+                                </li>
+
+                                <li>
+                                    <a href="#"><img src="/img/icon/search.png" alt=""></a>
+                                </li>
+                            </ul>
+                        </div>
+
+                        <div class="product__item__text">
+
+                            <h6>${product.name}</h6>
+
+                            <a href="#" class="add-cart">+ Add To Cart</a>
+
+                            <div class="rating">
+                                <i class="fa fa-star-o"></i>
+                                <i class="fa fa-star-o"></i>
+                                <i class="fa fa-star-o"></i>
+                                <i class="fa fa-star-o"></i>
+                                <i class="fa fa-star-o"></i>
+                            </div>
+
+                            <h5>$${product.price}</h5>
+
+                            <div class="product__color__select">
+                                <label for="pc-4">
+                                    <input type="radio" id="pc-4">
+                                </label>
+
+                                <label class="active black" for="pc-5">
+                                    <input type="radio" id="pc-5">
+                                </label>
+
+                                <label class="grey" for="pc-6">
+                                    <input type="radio" id="pc-6">
+                                </label>
+                            </div>
+                        </div>
+                    </div>
+                `;
+                    productList.appendChild(productItem);
+                });
+            })
+            .catch((error) => {
+                console.error("Error fetching product data:", error);
+            });
+    });
+}
+
+src="/js/jquery-3.3.1.min.js"
+src="/js/bootstrap.min.js"
+src="/js/jquery.nice-select.min.js"
+src="/js/jquery.nicescroll.min.js"
+src="/js/jquery.magnific-popup.min.js"
+src="/js/jquery.countdown.min.js"
+src="/js/jquery.slicknav.js"
+src="/js/mixitup.min.js"
+src="/js/owl.carousel.min.js"
+src="/js/main.js"
+src="/js/common.js"
+src="https://kit.fontawesome.com/c7429944f7.js" crossorigin="anonymous"
---
+++ .history/js/productList_20231115144543.js
@@ -0,0 +1,93 @@
+const filterProducts = (category) => {
+    window.location.assign(`/Customer/Product/productList.html?category=${category}`);
+}
+
+const renderData = async () => {
+	const response = await fetch("http://localhost:4001/api/products");
+	const data = await response.json();
+	const productList = document.getElementById("product-list");
+	let dataRender;
+	let params = new URL(document.location).searchParams;
+    let category = params.get("category"); 
+    console.log(category);
+
+	if (category === null || category === 'All') {
+		dataRender = data.products;
+	} else {
+		var filterData = await data.products.filter(
+			(item) => item.category === category
+		);
+        dataRender = filterData;
+
+        console.log(dataRender);
+	}
+    if(dataRender.length===0){
+        var listItem = document.createElement("div");
+        listItem.className = "col-lg-4 col-md-6 col-sm-6";
+        listItem.innerHTML = `
+        <p>Khong co san pham</p>
+    `;
+    productList.appendChild(listItem);
+    }else{
+        dataRender.forEach((item) => {
+            var listItem = document.createElement("div");
+            listItem.className = "col-lg-4 col-md-6 col-sm-6";
+            listItem.innerHTML = `
+            <div class="product__item">
+            <div class="product__item__pic">
+                <img src= "${item.image}"/>
+    
+                <ul class="product__hover">  
+                    <li>
+                        <a href="#"><img src="/img/icon/heart.png" alt=""></a>
+                    </li>
+    
+                    <li>
+                        <a href="#"><img src="/img/icon/compare.png" alt=""> <span>Compare</span></a>
+                    </li>
+    
+                    <li>
+                        <a href="#"><img src="/img/icon/search.png" alt=""></a>
+                    </li>
+                </ul>
+            </div>
+    
+            <div class="product__item__text">
+    
+                <h6>${item.name}</h6>
+    
+                <a href="#" class="add-cart">+ Add To Cart</a>
+    
+                <div class="rating">
+                    <i class="fa fa-star-o"></i>
+                    <i class="fa fa-star-o"></i>
+                    <i class="fa fa-star-o"></i>
+                    <i class="fa fa-star-o"></i>
+                    <i class="fa fa-star-o"></i>
+                </div>
+    
+                <h5>$${item.price}</h5>
+    
+                <div class="product__color__select">
+                    <label for="pc-4">
+                        <input type="radio" id="pc-4">
+                    </label>
+    
+                    <label class="active black" for="pc-5">
+                        <input type="radio" id="pc-5">
+                    </label>
+    
+                    <label class="grey" for="pc-6">
+                        <input type="radio" id="pc-6">
+                    </label>
+                </div>
+            </div>
+        </div>
+        `;
+        productList.appendChild(listItem);
+        });
+    }
+
+};
+renderData();
+
