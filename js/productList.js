9209c88178c2a0eaa3cba6a317e551c197984a09
Phuc Nguyen <tungaluoi2010@gmail.com>
2023-11-19 12:34:58 +0700

    Tuan fix product list

--- js/productList.js
+++ js/productList.js
@@ -1,51 +1,75 @@
+var url = new URL(window.location.href);
+if (!url.searchParams.has("filterParameter")) {
+  url.search += (url.search ? "&" : "?") + "filterParameter=filterValue";
+}
+
+// ============================== Filter ==============================
 // Filter Products
-const filterProducts = (category) => {
-  const newUrl = `/Customer/Product/productList.html?category=${category}`;
+const filterByCategory = (category) => {
+  url.searchParams.set("category", category);
+  const newUrl = url;
   history.pushState({ category }, null, newUrl);
   renderData();
 };
 
-const reFilterProducts = (all) => {
-  const newUrl = `/Customer/Product/productList.html?all`;
-  history.pushState({ all }, null, newUrl);
+// Filter Price
+const filterByPrice = (price1, price2) => {
+  url.searchParams.set("price1", price1);
+  url.searchParams.set("price2", price2);
+  const newUrl = url;
+  history.pushState({ price1, price2 }, null, newUrl);
   renderData();
 };
 
-const filterPrice = () => {
-  const newUrl = `/Customer/Product/productList.html?price=${price}`;
-  history.pushState({ price }, null, newUrl);
+// Filter Sizes
+const filterBySize = (size) => {
+  url.searchParams.set("size", size);
+  const newUrl = url;
+  history.pushState({ size }, null, newUrl);
   renderData();
 };
 
-// Render Data
+// ============================== Render Data ==============================
 const renderData = async () => {
   const response = await fetch("http://localhost:4001/api/products");
   const data = await response.json();
   const productList = document.getElementById("product-list");
-  let dataRender;
+  let dataRender = data.products;
   let params = new URL(document.location).searchParams;
-  let category = params.get("category");
-  let price = params.get("price");
-  let searchQuery = params.get("search");
 
-  // Set the search input value based on the query parameter
-  const searchInput = document.getElementById("search-input");
+  let searchQuery = params.get("search");
+  let category = params.get("category");
+  let price1 = params.get("price1");
+  let price2 = params.get("price2");
+  let size = params.get("size");
+
+  const searchInput = document.getElementById("search-input"); // Set the search input value based on the query parameter
   searchInput.value = searchQuery || ""; // Use empty string if searchQuery is null
 
-  if (category === null || category === "All") {
-    dataRender = data.products;
-  } else {
-    var filterData = await data.products.filter(
-      (item) => item.category === category
-      // (item) => item.category === category && item?.price >= 50 && item?.price <= 100
+  if (searchQuery) {
+    var filterData = dataRender.filter((item) =>
+      item.name.toLowerCase().includes(searchQuery.toLowerCase())
     );
     dataRender = filterData;
   }
 
-  if (searchQuery) {
-    dataRender = dataRender.filter((item) =>
-      item.name.toLowerCase().includes(searchQuery.toLowerCase())
+  if ((price1, price2)) {
+    var filterData = await dataRender.filter(
+      (item) => item.price <= price2 && item.price >= price1
     );
+    dataRender = filterData;
+  }
+
+  if (category) {
+    var filterData = await dataRender.filter(
+      (item) => item.category === category
+    );
+    dataRender = filterData;
+  }
+
+  if (size) {
+    var filterData = await dataRender.filter((item) => item.size === size);
+    dataRender = filterData;
   }
 
   productList.innerHTML = ""; // Clear the existing product list before rendering
@@ -62,8 +86,8 @@ const renderData = async () => {
       listItem.innerHTML = `
             <div class="product__item">
                 <div class="product__item__pic">
-                <a href="../Product/productDetail.html?id=${item._id}"><img src="${item.image}" style="height: 260px; width: 260px;"/></a>
-                    
+                    <a href="../Product/productDetail.html?id=${item._id}"><img src= "${item.image}"/></a>
+        
                     <ul class="product__hover">  
                         <li>
                             <a href="#"><img src="/img/icon/heart.png" alt=""></a>
@@ -116,19 +140,20 @@ const renderData = async () => {
   }
 };
 
-// Search by name
+// ============================== Search ==============================
 document.addEventListener("DOMContentLoaded", () => {
   const searchForm = document.getElementById("search-form");
 
   searchForm.addEventListener("submit", async (event) => {
     event.preventDefault();
     const searchInput = document.getElementById("search-input").value;
+    url.searchParams.set("search", searchInput);
     const newUrl = `/Customer/Product/productList.html?search=${searchInput}`;
 
     // Use the History API to update the URL without reloading the page
     history.pushState({ searchInput }, null, newUrl);
 
-    // Update the content based on the new search input
+    uncheckAllCheckboxes();
     await renderData();
   });
 
@@ -142,64 +167,164 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 
-  // Initial rendering
   renderData();
 });
 
+// ============================== Handle Checkbox Click ==============================
 // Handle Checkbox Click Category
 function handleCheckboxClickCategory(checkboxId, category) {
-  // Uncheck all checkboxes
   document
-    .querySelectorAll('.category-list input[type="checkbox"]')
+    .querySelectorAll('.category-list input[type="radio"]')
     .forEach(function (checkbox) {
       checkbox.checked = false;
     });
 
-  // Check the clicked checkbox
-  document.getElementById(checkboxId).checked = true;
+  document.getElementById(checkboxId).checked = true; // Check the clicked checkbox
 
-  filterProducts(category);
+  filterByCategory(category);
 }
 
 // Handle Checkbox Click Price
 function handleCheckboxClickPrice(checkboxId) {
-  // Uncheck all checkboxes
   document
-    .querySelectorAll('.shop__sidebar__price input[type="checkbox"]')
+    .querySelectorAll('.shop__sidebar__price input[type="radio"]')
     .forEach(function (checkbox) {
       checkbox.checked = false;
     });
 
-  // Check the clicked checkbox
-  document.getElementById(checkboxId).checked = true;
+  document.getElementById(checkboxId).checked = true; // Check the clicked checkbox
+
+  filterByPrice(price1, price2);
 }
 
-// Add to cart
-const addToCart = (productId) => {
-  function getBearerToken() {
-    const userString = localStorage.getItem("user");
+// Handle Checkbox Click Size
+function handleCheckboxClickCategory(checkboxId, size) {
+  document
+    .querySelectorAll('.shop__sidebar__size input[type="radio"]')
+    .forEach(function (checkbox) {
+      checkbox.checked = false;
+    });
 
-    if (userString) {
-      const userData = JSON.parse(userString);
-      return userData.access_token || null;
+  document.getElementById(checkboxId).checked = true; // Check the clicked checkbox
+
+  filterBySize(size);
+}
+
+// ============================== Uncheck All Checkboxes ==============================
+function uncheckAllCheckboxes() {
+  document
+    .querySelectorAll('.category-list input[type="radio"]')
+    .forEach(function (checkbox) {
+      checkbox.checked = false;
+    });
+
+  document
+    .querySelectorAll('.shop__sidebar__price input[type="radio"]')
+    .forEach(function (checkbox) {
+      checkbox.checked = false;
+    });
+
+  document
+    .querySelectorAll('.shop__sidebar__size input[type="radio"]')
+    .forEach(function (checkbox) {
+      checkbox.checked = false;
+    });
+}
+
+// ============================== Clear Filter ==============================
+// Clear All Filter
+function clearAllFilter() {
+  var url = "/Customer/Product/productList.html";
+  uncheckAllCheckboxes();
+  deleteAllParams();
+  // Get the current scroll position
+  var startY = window.scrollY;
+
+  // Scroll to the top of the page with animation
+  var scrollStep = Math.PI / (1 / 10);
+  var cosParameter = startY;
+  var scrollCount = 0;
+
+  function step() {
+    window.scrollTo(0, cosParameter);
+    cosParameter = cosParameter - scrollStep;
+    scrollCount++;
+    if (cosParameter >= 0) {
+      requestAnimationFrame(step);
+    } else {
+      window.scrollTo(0, 0);
+      document.body.classList.remove("scroll-up");
     }
-    return null;
   }
 
-  const token = getBearerToken();
+  document.body.classList.add("scroll-up"); // Add a class to the body to trigger the slide-up effect
+  requestAnimationFrame(step); // Call the animation function
+  history.pushState(null, null, url); // Update the URL without triggering a page refresh
+  renderData();
+}
 
-  if (!token) {
-    window.location.href = "/Customer/LoginAndRegister/Login/Login.html";
-  }
+// Clear Filter Category
+function clearFilterCategory() {
+  var url = new URL(window.location.href);
+  url.searchParams.delete("category");
+  document
+    .querySelectorAll('.category-list input[type="radio"]')
+    .forEach(function (checkbox) {
+      checkbox.checked = false;
+    });
+  history.pushState(null, "", url.toString());
+  renderData();
+}
 
+// Clear Filter Price
+function clearFilterPrice() {
+  var url = new URL(window.location.href);
+  url.searchParams.delete("price1");
+  url.searchParams.delete("price2");
+  document
+    .querySelectorAll('.shop__sidebar__price input[type="radio"]')
+    .forEach(function (checkbox) {
+      checkbox.checked = false;
+    });
+  history.pushState(null, "", url.toString());
+  renderData();
+}
+
+// Clear Filter Size
+function clearFilterSize() {
+  var url = new URL(window.location.href);
+  url.searchParams.delete("size");
+  document
+    .querySelectorAll('.shop__sidebar__size input[type="radio"]')
+    .forEach(function (checkbox) {
+      checkbox.checked = false;
+    });
+  history.pushState(null, "", url.toString());
+  renderData();
+}
+
+// ============================== Delete Params ==============================
+function deleteAllParams() {
+  url.searchParams.delete("search");
+  url.searchParams.delete("category");
+  url.searchParams.delete("price1");
+  url.searchParams.delete("price2");
+  url.searchParams.delete("size");
+}
+
+// ==============================  Add To Cart ==============================
+const addToCart = (productId) => {
+  console.log(`${productId}`);
+  const user = JSON.parse(localStorage.getItem("user"));
+  //Make a POST request to your cart API endpoint
   fetch("http://localhost:4001/api/carts", {
     method: "POST",
     headers: {
-      Authorization: `Bearer ${token}`,
       "Content-Type": "application/json",
     },
     body: JSON.stringify({
       _id: productId,
+      userId: user._id,
     }),
   })
     .then((response) => response.json())
@@ -213,7 +338,7 @@ const addToCart = (productId) => {
     });
 };
 
-// Toast
+// ==============================  Toast ==============================
 function showSuccessToast() {
   toast({
     title: "Succces!",
