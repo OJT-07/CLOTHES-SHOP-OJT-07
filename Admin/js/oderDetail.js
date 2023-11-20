efee82ff0df1a66a0fb947dacd1a381cfdcc0498
lamvbg <lamvbgcd191320@fpt.edu.vn>
2023-11-20 22:48:50 +0700

    TT-33

--- Admin/js/oderDetail.js
+++ Admin/js/oderDetail.js
@@ -9,9 +9,11 @@ function getBearerToken() {
 
 async function fetchData() {
   try {
+    const urlParams = new URLSearchParams(window.location.search);
+    const id = urlParams.get("id");
     const token = getBearerToken();
     const response = await fetch(
-      `http://localhost:4001/api/orders/655af8ba1ae605a333ed23b9`,
+      `http://localhost:4001/api/orders/${id}`,
       {
         method: "GET",
         headers: {
@@ -38,41 +40,74 @@ async function renderData() {
 
     let totalPrice = 0;
 
-    // for (const cartItem in data.cart_items) {
-    //     // console.log(`Order ID: ${order._id}, Cart Item ID: ${cartItem._id}, Cart Item Price: ${cartItem.cart_item_price}`);
-    //     totalPrice += cartItem.cart_item_price;
-    // }
+    for (const cartItem in data.cart_items) {
+      // console.log(`Order ID: ${order._id}, Cart Item ID: ${cartItem._id}, Cart Item Price: ${cartItem.cart_item_price}`);
+      totalPrice += cartItem.cart_item_price;
+    }
 
-    // const array = data.order;
+    const array = data.data;
 
-    // console.log(array);
+    for (let i = 0; i < array.cart_items.length; i++) {
+      totalPrice += array.cart_items[i].cart_item_price;
 
-    // for (let i = 0; i < array.cart_items.length; i++) {
-    //   totalPrice += array.cart_items[i].cart_item_price;
-    // }
 
-    const time = data.createdAt;
+    }
 
+    const time = data.data.createdAt;
     const dateObject = new Date(time);
-
     const formattedDate = `${dateObject.getFullYear()}-${(
       dateObject.getMonth() + 1
     )
       .toString()
       .padStart(2, "0")}-${dateObject.getDate().toString().padStart(2, "0")}`;
-    console.log(formattedDate);
+    const code = data.data.code;
 
-    const code = data.code;
+    const tableBody = document.getElementById("productTeam");
 
-    console.log(totalPrice);
+    data.data.cart_items.forEach((productDetail, index) => {
+      const tr = document.createElement('tr');
+      const uniqueId = `name_${index}`; // Generate a unique ID
+      tr.innerHTML = `
+        <td>
+          <div class="d-flex mb-2">
+            <div class="flex-shrink-0">
+              <img src="${productDetail.productId.image}" alt="" width="35" class="img-fluid">
+            </div>
+            <div class="flex-lg-grow-1 ms-3">
+              <h6 class="small mb-0"><a href="#" id="${uniqueId}" class="text-reset">${productDetail.productId.name}</a></h6>
+              <span class="small">Color: ${productDetail.color}</span>
+            </div>
+          </div>
+        </td>
+        <td>${productDetail.quantity}</td>
+        <td class="text-end">$${productDetail.cart_item_price}.00</td>
+      `;
+    
+      tableBody.appendChild(tr);
+    });
+    
+    const address = data.data.userId.address;
+    const phone = data.data.userId.phone;
+    const name = data.data.userId.name;
+
 
     document.getElementById("TotalPrice1").innerHTML =
       "$" + totalPrice.toFixed(2);
     document.getElementById("TotalPrice2").innerHTML =
       "$" + totalPrice.toFixed(2);
+      document.getElementById("TotalPrice3").innerHTML =
+      "$" + totalPrice.toFixed(2);
+
     document.getElementById("timeCreate").innerHTML = formattedDate;
     document.getElementById("code").innerHTML = "#" + code;
+    document.getElementById("address").innerHTML = address;
+    document.getElementById("address1").innerHTML = address;
+    document.getElementById("phone").innerHTML = "Phone: " + phone;
+    document.getElementById("phone1").innerHTML = "Phone: " + phone;
     document.getElementById("name").innerHTML = name;
+    document.getElementById("name1").innerHTML = name;
+    document.getElementById("FedEx").innerHTML = "FF" + code;
+
   } catch (error) {
     console.error("Error fetching or rendering data:", error);
   }
